ba7d2750b0b9bd854c5c3d03b9381298064b1f83
ghulammurtaza0 <140093107+ghulammurtaza0@users.noreply.github.com>
2023-08-01 18:20:47 +0500

    Add files via upload

---
+++ assignment1.ts
@@ -0,0 +1,26 @@
+var message:string=("Hey Abu Hurairah,I have started learning TYPESCRIPT");
+console.log(message);
+
+var Name:string=("Abu Hurairah");
+console.log(Name);
+
+var userName:string=("num");
+var num1:number= (10);
+var num2:number= (20);
+var num3:number= (30);
+var num4:number= (40);
+var num5:number= (50);
+var num6:number= (60);
+var num7:number= (70);
+var num8:number= (80);
+var num9:number= (90);
+var num10:number= (100);
+var adding:number= (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10);
+var subtraction:number= (num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 -num10);
+var multiplying:number= (num1* num2* num3* num4* num5* num6* num7* num8* num9* num10);
+var division:number=(num1 % num5);
+console.log(adding);
+console.log(subtraction);
+console.log(multiplying);
+console.log(division);
+
